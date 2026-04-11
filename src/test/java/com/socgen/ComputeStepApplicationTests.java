e873153727b24b9d8c4d29853580061fe38bd3f4
gowtham76 <gowthamswaroop76@gmail.com>
2026-04-11 23:14:26 +0530

    first commit

---
+++ src/test/java/com/socgen/ComputeStepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputeStepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
